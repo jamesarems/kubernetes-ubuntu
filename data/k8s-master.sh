cda1686f4ab3236e48e3af5cf0795515e1c1aaa6
James PS <jamesarems@gmail.com>
2019-11-02 11:41:15 +0400

    Bug Fix : Remove NAT IP from node
    Remove NAT IP from node environment file.

--- data/k8s-master.sh
+++ data/k8s-master.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+SYSIP=`ip -4 addr | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | sed -n 3p`
+
 echo "[1] k8s Master Setup"
 
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=192.168.33.10 > /dev/null
@@ -7,6 +9,8 @@ mkdir -p /home/vagrant/.kube
 sudo cp -i /etc/kubernetes/admin.conf /home/vagrant/.kube/config
 sudo chown vagrant:vagrant /home/vagrant/.kube/config
 sudo -H -u vagrant bash -c 'kubectl apply -f https://docs.projectcalico.org/v3.8/manifests/canal.yaml' > /dev/null
+sudo sed -i "s/--network-plugin=cni/--network-plugin=cni --node-ip=$SYSIP/g" /var/lib/kubelet/kubeadm-flags.env
+sudo systemctl restart kubelet
 sudo -H -u vagrant bash -c 'kubeadm token create --print-join-command' > /vagrant_data/join
 
 echo "[2] Setup Completed"
