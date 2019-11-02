c0f056b172a1ba411c8f1ecaaed350b0d07e20b9
James PS <jamesarems@gmail.com>
2019-11-02 11:42:43 +0400

    Bug Fix: Removed NAT IP from node env

--- data/k8s-slave.sh
+++ data/k8s-slave.sh
@@ -1,7 +1,10 @@
 #/bin/bash
+SYSIP=`ip -4 addr | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | sed -n 3p`
 
 echo "[1] k8s worker node - Joining to cluster"
 
 bash /vagrant_data/join > /dev/null
+sudo sed -i "s/--network-plugin=cni/--network-plugin=cni --node-ip=$SYSIP/g" /var/lib/kubelet/kubeadm-flags.env
+sudo systemctl restart kubelet
 
 echo "[2] Success . ssh km1 to manage cluster"
